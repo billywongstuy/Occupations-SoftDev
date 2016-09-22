fe2cc4e578ef1212f54a71c9afeb8f36b1c388a1
Billy Wong <bwong5@stuy.edu>
2016-09-22 17:41:45 -0400

    fixed to be more terminal friendly

--- Occupations.py
+++ Occupations.py
@@ -2,12 +2,11 @@ import csv
 import random
 
 sheet = csv.reader(open("occupations.csv"))
-sheet.next()
 jobs = {}
 
 
 for row in sheet:
-    if row[0] != "Total":
+    if row[0] != "Total" and row[1] != "Percentage":
         job = row[0]
         percent = float(row[1])
         jobs[job] = percent
@@ -17,15 +16,13 @@ for row in sheet:
 rand = float(random.randrange(998))/10
 tracker = rand
 
-print "Random: " + str(rand)
-
 
 #This is kind of unefficient because the loop doesn't
 #stop until the last value no matter what
 for key in jobs:
     if tracker != 20000:
         if jobs[key] >= tracker:
-            print key 
+            print(key) 
             tracker = 20000
         else:
             tracker -= jobs[key]
